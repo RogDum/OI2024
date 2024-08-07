eb1ffffd11da3cf696a3fb6da112da4c45968321
RogDum <m13393551888@outlook.com>
2024-08-07 09:46:09 +0800

    first commit

---
+++ p1801.cpp
@@ -0,0 +1,18 @@
+#include<queue>
+#include<iostream>
+#include<string>
+using namespace std;
+
+priority_queue<int, greater<int>> top;
+priority_queue<int> bot;
+string cmd;
+int i;
+
+int main(){
+    while(cin>>cmd){
+        if(cmd[0]=='A'){
+            int x=cmd[4];
+            
+        }
+    }
+}
